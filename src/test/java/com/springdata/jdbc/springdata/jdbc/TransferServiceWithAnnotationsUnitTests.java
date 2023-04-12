eb14c4b353943363119fc40a86b187c7509e23ba
sanix <laa252159@gmail.com>
2023-04-12 21:34:43 +0400

    TransferServiceWithAnnotationsUnitTests

---
+++ src/test/java/com/springdata/jdbc/springdata/jdbc/TransferServiceWithAnnotationsUnitTests.java
@@ -0,0 +1,48 @@
+package com.springdata.jdbc.springdata.jdbc;
+
+import com.springdata.jdbc.springdata.jdbc.model.Account;
+import com.springdata.jdbc.springdata.jdbc.repositories.AccountRepository;
+import com.springdata.jdbc.springdata.jdbc.services.TransferService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class TransferServiceWithAnnotationsUnitTests {
+
+    @Mock
+    private AccountRepository accountRepository;
+
+    @InjectMocks
+    private TransferService transferService;
+
+    @Test
+    @DisplayName("Test the amount is transferred +\n" + "from one account to another if no exception occurs.")
+    public void moneyTransferHappyFlow() {
+        Account sender = new Account();
+        sender.setId(1);
+        sender.setAmount(new BigDecimal(1000));
+
+        Account destination = new Account();
+        destination.setId(2);
+        destination.setAmount(new BigDecimal(1000));
+
+        given(accountRepository.findById(sender.getId())).willReturn(Optional.of(sender));
+        given(accountRepository.findById(destination.getId())).willReturn(Optional.of(destination));
+
+        transferService.transferMoney(sender.getId(), destination.getId(), new BigDecimal(300));
+
+        verify(accountRepository).changeAmount(1, new BigDecimal(700));
+        verify(accountRepository).changeAmount(2, new BigDecimal(1300));
+    }
+}
